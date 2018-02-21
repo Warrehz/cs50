363a2db9eaed83db655ae6f6a948b4d8de77044a
Mical Juarez <mical.juarez@gmail.com>
2018-02-20 23:42:46 -0600

    lecture1 notes and sample C code

---
+++ lecture1.c
@@ -0,0 +1,21 @@
+// Simple hello world program
+#include <stdio.h>
+
+int main(void)
+{
+    // Prints hello, world once
+    printf("hello, world\n");
+
+    // Loop that forever prints hello world
+    while (true)
+    {
+        printf("hello, world\n");
+    }
+
+    // Loop that prints hello, world 50 times
+    for (int i = 0; i < 50; i++)
+    {
+        printf("hello, world\n");
+    }
+
+}

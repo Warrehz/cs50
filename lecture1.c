4a65879c41079e51d58ec3e095122113112be3c8
Mical Juarez <mical.juarez@gmail.com>
2018-02-22 00:57:28 -0600

    implemented newmario of pset1

--- lecture1.c
+++ lecture1.c
@@ -1,5 +1,6 @@
 // Simple hello world program
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
@@ -21,6 +22,36 @@ int main(void)
     // Pset 1
 
     // Implementation of Mario - more comfortable
-    
+    int height;
+
+    do
+    {
+        printf("Enter a height: ");
+        height = GetInt();
+    } while (height < 1 || height >= 24);
+
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int m = 0; m < i + 1; m++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
 
 }

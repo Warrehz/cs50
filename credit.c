a94a7254dba14ec2169617fbe6d646cbf4b3100b
Mical Juarez <mical.juarez@gmail.com>
2018-02-28 17:22:56 -0600

    can get num in each digit, bulk logic there, need to add final checks

--- credit.c
+++ credit.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <cs50.h>
 
-#include <stdio.h>
-#include <cs50.h>
-
 int main(void)
 {
 
-    long long ccNumber;
-    int count = 0;
+    long long ccnumber;
+    int addSum = 0;
+    int onesDigit, tensDigit, prodSum;
 
     // Get user credit card number
     printf("Enter Credit Card Number: ");
-    ccNumber = GetLongLong();
+    ccnumber = GetLongLong();
 
-
-    // Loop to iterate over credit card number
-    while (ccNumber)
+    // Iterate over ccnumber and add every other num starting with last
+    for (int i = ccnumber; i > 0; i /= 100)
     {
-        int digit = ccNumber % 10;
+        addSum += i % 10;
+    }
 
+    // Iterate over ccnumber, multiply every other num starting with second to last
+    for (int i = ccnumber; i > 0; i /= 100)
+    {
+        int digit = i /10 % 10;
 
+        prodSum = digit * 2;
 
-        ++count;
-        ccNumber /= 10;
+        // Calculate the num in each digit space
+        onesDigit = prodSum % 10;
+        tensDigit = (prodSum - onesDigit) / 10;
+
+        printf("%i\n", onesDigit);
+        printf("%i\n", tensDigit);
+
     }
 
     return 0;

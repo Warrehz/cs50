80838c40e9e5ce5c65a0e137eabc1427904d203e
Mical Juarez <mical.juarez@gmail.com>
2018-03-01 00:46:42 -0600

    it's ugly but the validator works, just have to check which CC is used

--- credit.c
+++ credit.c
@@ -1,4 +1,8 @@
+/* Simple implementation of the Luhn algorithm to check for valid credit card
+   numbers */
+
 #include <stdio.h>
+#include <math.h>
 #include <cs50.h>
 
 int main(void)
@@ -6,32 +10,71 @@ int main(void)
 
     long long ccnumber;
     int addSum = 0;
-    int onesDigit, tensDigit, prodSum;
+    int digitSum = 0;
+    int digit = 0;
+    int digits = 0;
+    int onesDigit, tensDigit, prodSum, finalSum;
 
     // Get user credit card number
-    printf("Enter Credit Card Number: ");
-    ccnumber = GetLongLong();
-
-    // Iterate over ccnumber and add every other num starting with last
-    for (int i = ccnumber; i > 0; i /= 100)
+    do
     {
-        addSum += i % 10;
+        printf("Enter Credit Card Number: ");
+        ccnumber = GetLongLong();
+    }
+    while(ccnumber < 0);
+
+
+    long long tempcc = ccnumber;
+
+    // Find num of digits in CC number
+    while (tempcc >= 1)
+    {
+        tempcc = tempcc / 10;
+        ++digits;
     }
 
-    // Iterate over ccnumber, multiply every other num starting with second to last
-    for (int i = ccnumber; i > 0; i /= 100)
+    tempcc = ccnumber;
+
+    // Find sum of every other digit starting with last
+    for (int i = 0; i < digits; i++)
     {
-        int digit = i /10 % 10;
+        addSum += tempcc % 10;
+        tempcc /= 100;
 
+    }
+
+    tempcc = ccnumber;
+
+    for (int i = 0; i < floor(digits / 2); i++)
+    {
+        if (i == 0)
+        {
+            tempcc /= 10;
+        }
+        else
+        {
+            tempcc /= 100;
+        }
+
+        digit = tempcc % 10;
         prodSum = digit * 2;
-
-        // Calculate the num in each digit space
         onesDigit = prodSum % 10;
         tensDigit = (prodSum - onesDigit) / 10;
+        digitSum += onesDigit + tensDigit;
 
-        printf("%i\n", onesDigit);
-        printf("%i\n", tensDigit);
+    }
 
+    finalSum = digitSum + addSum;
+
+    if (finalSum % 10 != 0 || ccnumber == 0)
+    {
+        printf("WARNING: NOT A VALID CARD\n");
+    }
+    else
+    {
+        if (digit == 4) {
+            printf("VISA\n");
+        }
     }
 
     return 0;

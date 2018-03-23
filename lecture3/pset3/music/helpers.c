72ce1292a1d8bd7e11ee28ecca04073e881d769a
Mical Juarez <mical.juarez@gmail.com>
2018-03-23 00:08:09 -0500

    implemented is_rest and duration functions

--- lecture3/pset3/music/helpers.c
+++ lecture3/pset3/music/helpers.c
@@ -1,13 +1,21 @@
 // Helper functions for music
 
 #include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
+
+    int num = fraction[0];
+    int den = fraction[2];
+    int duration = num * 8 / den;
+
+    return duration;
+
 }
 
 // Calculates frequency (in Hz) of a note
@@ -19,5 +27,12 @@ int frequency(string note)
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    if (strncmp(s, "", 1) < 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
